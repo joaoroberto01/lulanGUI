3f99de54d6431e5df0260c010b60ce2de8b29bf9
joaoroberto <joaoroberto.gc.01@gmail.com>
2023-09-02 17:57:05 -0300

    Add linux/windows icon

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        icon: path.join(__dirname, 'app.ico')
     })
 
     win.loadFile('pages/main.html')
