80be77ccdd0786b92d28c96c323946bdb4bc0118
joaocavina <joaoroberto.gc.01@gmail.com>
2023-11-10 14:25:51 -0300

    Improvements

--- index.js
+++ index.js
@@ -1,31 +1,73 @@
-const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const { app, BrowserWindow, shell, dialog, ipcMain, nativeImage, Menu } = require('electron');
 const path = require('path');
+const { execSync, spawnSync } = require('child_process');
+const fs = require('fs');
 
-const nativeImage = require('electron').nativeImage;
+let window;
 
+//if (!app.isPackaged) {
+//    try {
+//        require('electron-reloader')(module);
+//    } catch {}
+//}
 const createWindow = () => {
-    const win = new BrowserWindow({
+    window = new BrowserWindow({
         width: 800,
         height: 600,
         icon: nativeImage.createFromPath(path.join(__dirname, 'app.png')),
         autoHideMenuBar: true,
         
         webPreferences: {
-            nodeIntegration: true,
             preload: path.join(__dirname, 'preload.js')
         }
     })
-
-    win.loadFile('pages/main.html')
+    window.loadFile('pages/main.html');
 }
+// Menu.setApplicationMenu(null);
 
 app.whenReady().then(() => {
     createWindow();
 
     ipcMain.handle('dialog:open', async () => {
-        const { canceled, filePaths } = await dialog.showOpenDialog()
-        if (!canceled) {
-            return filePaths[0];
+        return dialog.showOpenDialogSync(window)
+    });
+
+    ipcMain.handle('fs:read', (event, path) => {
+        return fs.readFileSync(path, 'utf-8');
+    });
+
+    ipcMain.handle('fs:write', (event, path, content) => {
+        if (!path) {
+            path = dialog.showSaveDialogSync(window);
+            if (!path) return;
         }
+        fs.writeFileSync(path, content, 'utf-8')
+
+        return path;
+    })
+
+    ipcMain.handle('compiler:compile', async (event, path) => {
+        let lulangCompiler = spawnSync('java', ['-jar', 'compiler.jar', path]);
+
+        return {
+            message: lulangCompiler.stdout.toString(),
+            error: lulangCompiler.stderr.toString()
+        }
+//        if (lulangCompiler.stderr) {
+//            return lulangCompiler.stderr.toString();
+//        }
+//
+//        return lulangCompiler.stdout.toString();
+
+        //STREAM FOR VIRTUAL MACHINE
+//        lulangCompiler.stdout.on('data', (data) => {
+//            console.log(data)
+//            window.webContents.send('compiler:on-message', data.toString());
+//        });
+//
+//        lulangCompiler.stderr.on('data', (data) => {
+//            console.log(data.toString())
+//            window.webContents.send('compiler:on-error', data.toString());
+//        });
     });
 })
